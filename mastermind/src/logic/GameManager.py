c9e4c1be210d15663731cfa6e42044554b3503b6
Mirco Kraenz <prosingularity@yahoo.de>
2017-07-28 16:13:03 +0200

    #3 ADD procedure to exit instead of new game

--- mastermind/src/logic/GameManager.py
+++ mastermind/src/logic/GameManager.py
@@ -13,23 +13,30 @@ class GameManager(object):
         self.goal_input_handler = goal_input_handler
         self.evaluator = evaluator
         
+
+    def new_game_or_exit(self):
+        inputstr = raw_input('Press Return key to start new game or type "exit" and hit Return to exit.')
+        if inputstr != "exit":
+            self.start_new_game()
+        print('Exit game. See you next time.')
+    
+    
     def start_game(self):
         self.goal_input_handler.manage()
         
-        for _ in xrange(ROUND_NUMBER):
+        for curr_round in xrange(ROUND_NUMBER):
+            print('Round %s of %s') % (curr_round+1, ROUND_NUMBER)
             self.player_input_handler.manage()
             self.player_codes.manage()
             self.evaluator.manage()
             if self.evaluator.is_won():
                 print("CONGRATULATIONS!")
-                raw_input("Press Return key to start new game.")
-                self.start_new_game()
+                self.new_game_or_exit()
+                break
                         
-            # TODO: might be unneccessary
             elif self.evaluator.is_lost():
-                print("Too Bad....")
-                raw_input("Press Return key to start new game.")
-                self.start_new_game()
+                print("You can do it! Try again.")
+                self.new_game_or_exit()
                 break
 
     def start_new_game(self):
