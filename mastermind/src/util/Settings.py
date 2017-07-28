56a6292ff7bddf951af412decf832042a497e074
Mirco Kraenz <prosingularity@yahoo.de>
2017-07-29 00:23:39 +0200

    #7 ADD ViewSettings RENAME full name of colors in Settings

--- mastermind/src/util/Settings.py
+++ mastermind/src/util/Settings.py
@@ -6,13 +6,12 @@ Created on 04.09.2014
 
 COLORS = {
           "black": 0,
-          "b": 1,
-          "r": 2,
-          "y": 3,
+          "blue": 1,
+          "red": 2,
+          "yellow": 3,
           "brown": 4,
-          "g": 5,
-          "white": 6,
-          "orange": 7,
+          "green": 5,
+          "orange": 6,
           }
 
 # the number of stones per combination
---
+++ mastermind/src/view/ViewSettings.py
@@ -0,0 +1,17 @@
+'''
+Created on 28.07.2017
+
+@author: Mirco
+'''
+COLORS_IN_RGB = {'black': (0, 0, 0),
+                 'blue': (0,0,255),
+                 'red': (255, 0, 0),
+                 'yellow': (0,255,0),
+                 'brown': (139,69,19),
+                 'green': (0,128,0),
+                 'white': (255, 255, 255),
+                 'orange': (255,140,0),
+                 }
+
+SCREEN_DIMENSIONS = (800, 600)
+BLOCK_SIZE = 45 # dimensions of buttons below to choose colors from
