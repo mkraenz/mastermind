ddda5a37b235cf0f384dff817ca060ff378b8747
Mirco Kraenz <prosingularity@yahoo.de>
2017-08-05 22:07:16 +0200

    #29 ADD resize functionality UPDATE use python standard exit

--- mastermind/src/view/Game.py
+++ mastermind/src/view/Game.py
@@ -8,31 +8,38 @@ import pygame
 from ViewSettings import COLORS_TO_RGB
 from view import ViewSettings
 from view.SceneManager import SceneManager
+import sys
+from pygame.constants import VIDEORESIZE
 
 def game_loop(surface):
     game_running = True
     manager = SceneManager()
+    clock = pygame.time.Clock()
     
     while game_running:
         if pygame.event.get(pygame.QUIT):
             game_running = False
             return
+        for event in pygame.event.get(VIDEORESIZE):
+            surface = pygame.display.set_mode((event.w, event.h), pygame.RESIZABLE)
         manager.scene.handle_events(pygame.event.get())
         manager.scene.update()
         manager.scene.render(surface)
         pygame.display.update()
+        clock.tick(60)
     
 def getConfiguredMainSurface():
-    surface = pygame.display.set_mode(ViewSettings.SCREEN_DIMENSIONS)
+    surface = pygame.display.set_mode(ViewSettings.SCREEN_DIMENSIONS, pygame.RESIZABLE)
     surface.fill(COLORS_TO_RGB['white'])
     pygame.display.set_caption('Mastermind. App by ProSingularity.')
     return surface
 
 def main():
+        
     pygame.init()
     surface = getConfiguredMainSurface()
     game_loop(surface)
     pygame.quit()
-    quit()
+    sys.exit()
     
 main()
